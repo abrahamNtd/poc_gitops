7a45165d922600d2d73bcb6136eaa1073d690af4
Abraham Ntd <agmc22mx@gmail.com>
2023-03-16 03:29:40 -0600

    [@abrahamNtd basic_config] testing post deploy actions

--- dev/scripts/post_deploy_actions.py
+++ dev/scripts/post_deploy_actions.py
@@ -19,14 +19,17 @@ def list_directory(directory):
 		print(" - " + item)
 
 def delete_directory(directory):
-	if os.path.isdir(directory):
-		shutil.rmtree(directory)
-		print("\n-- Deleted '%s' directory successfully" % directory)
+	print("To do")
+	#if os.path.isdir(directory):
+		#shutil.rmtree(directory)
+		#print("\n-- Deleted '%s' directory successfully" % directory)
 
 if __name__ == '__main__':
 	if len(sys.argv) > 2:
 		action = sys.argv[1]
 		if action == "list_directory":
 			list_directory(sys.argv[2])
+		elif action == "delete_directory":
+			delete_directory(sys.argv[2])
 		else:
 			print("Action not valid")
